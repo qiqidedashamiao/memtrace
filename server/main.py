a57e112a6fec2b03c8ab31dfe887e17415b50b6e
qiqidedashamiao <zl_11191224@163.com>
2024-08-16 07:33:39 +0800

    服务端更新

--- server/main.py
+++ server/main.py
@@ -6,8 +6,26 @@ import struct
 
 # 定义结构体的格式字符串
 # < 表示小端序，b 表示 int8，h 表示 int16，I 表示 unsigned int，q 表示 64 位指针 (void*)
-# 结构体总大小 = 1 + 1 + 2 + 4 + 4 + 8 + 8 + 8 + (8 * 5) = 56 bytes
-struct_format = '<bbhIiqqq5q'
+# 结构体总大小 = 1 + 1 + 2 + 4 + 4 + 8 + 8 + 8 + (8 * 5) = 76 bytes
+#struct_format = '<bbhIiqqq5q'
+#结构体总大小 = 1 + 1 + 2 + 4 + 4 + 8 + 8 + 8 = 36 bytes
+struct_format = '<bbhIiqqq'
+# 定义结构格式
+#struct_format = 'I 4s f'  # 例如：一个无符号整数，一个4字节字符串，一个浮点数
+struct_size = struct.calcsize(struct_format)
+#存储struct_format格式的内存记录
+memory_records = []
+# 打包数据
+packed_data = b''.join([struct.pack(struct_format, *data) for data in data_list])
+# 将打包的数据存储到文件中
+with open('memory_records.bin', 'wb') as f:
+    f.write(packed_data)
+
+# 从文件中读取数据
+with open('memory_records.bin', 'rb') as f:
+    read_data = f.read()
+# 解包数据
+unpacked_data_list = [struct.unpack(struct_format, read_data[i:i+struct_size]) for i in range(0, len(read_data), struct_size)]
 
 
 
@@ -35,7 +53,7 @@ def parse_mem_log_info(data):
 # 示例数据（假设你已经接收到了 56 字节的数据）
 data = b'\x01\x02\x34\x12\x78\x56\x34\x12\x9A\xBC\xDE\xF0\x08\x00\x00\x00' \
        b'\x10\x20\x30\x40\x50\x60\x70\x80\x10\x20\x30\x40\x50\x60\x70\x80' \
-       b'\x10\x20\x30\x40\x50\x60\x70\x80\x10\x20\x30\x40\x50\x60\x70\x80'
+       b'\x10\x20\x30\x40'
 
 parsed_data = parse_mem_log_info(data)
 print(parsed_data)
@@ -45,14 +63,6 @@ def handle_client(conn, addr):
     print(f"Connected by {addr}")
     try:
         while True:
-            
-            
-
-
-
-
-            
-
             data = conn.recv(1024).decode()
             if not data:
                 break
