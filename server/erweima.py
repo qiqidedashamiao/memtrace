808b35c126f642578b2cd0f25b6e47c493daad6e
qiqidedashamiao <zl_11191224@163.com>
2024-08-17 10:43:46 +0800

    模拟解析远程客户端产生的二维码并显示为图片

--- server/erweima.py
+++ server/erweima.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding=utf-8
 
 import sys
 import subprocess
@@ -20,8 +18,8 @@ def installPipModule(module):
 
 
 # 先安装fabric模块
-installPipModule("qrcode")
-installPipModule("Image")
+#installPipModule("qrcode")
+#installPipModule("Image")
 
 import qrcode
 from PIL import Image
@@ -34,22 +32,15 @@ def generate_qr_in_console(data):
         version=1,  # 控制二维码的大小
         error_correction=qrcode.constants.ERROR_CORRECT_L,  # 控制二维码的容错率
         box_size=1,  # 控制输出的大小，这里设置为1是为了控制台输出
-        border=4,  # 控制边框的宽度
+        border=0,  # 控制边框的宽度
     )
     
     qr.add_data(data)
     qr.make(fit=True)
 
     # 将二维码转化为控制台可显示的字符图案
-    qr.print_ascii(invert=True)  # invert=True 反转黑白显示效果
+    qr.print_ascii(invert=False)  # invert=True 反转黑白显示效果
 
-
-def display_qr_image(image_data):
-    # 将二进制图像数据转换为 PIL 图像对象
-    img = Image.open(io.BytesIO(image_data))
-    
-    # 显示图像
-    img.show()
 
 # 假设 `received_image_data` 是从客户端接收到的二进制图像数据
 # received_image_data = ...
@@ -59,7 +50,7 @@ def display_qr_image(image_data):
 
 
 # 要展示的数据
-data = "https://www.example.com"
+data = "aaaaaa"
 
 # 生成并在控制台展示二维码
 generate_qr_in_console(data)
--- server/ssh.py
+++ server/ssh.py
@@ -1,74 +1,33 @@
-# import io
-# from logging import warn
-# from threading import local
-# import sys
-# import subprocess
-# import time
-# import importlib.util
-
-# def installPipModule(module):
-#     if importlib.util.find_spec(module) is not None:
-#         return
-#     print(f"\n{module}模块未安装，先安装{module}模块\n")
-#     try:
-#         cmd = f"pip install {module}"
-#         print(cmd)
-#         subprocess.call(cmd, shell=True)
-#     except subprocess.CalledProcessError as e:
-#         print(f"\n安装{module}模块时异常：\n", e.output)
-#         sys.exit(1)
-#     print(f"\n{module}模块安装成功\n")
-
-# # 先安装fabric模块
-# installPipModule("fabric")
-# from fabric import Connection
-# import paramiko
-
-# def genRemoteConnectionFunc(host, pwd):
-#     con = Connection(host=f"{host}", connect_kwargs={'password': pwd})
-#     try:
-#         con.open()
-#     except Exception:
-#         print(f"远程节点创建失败，hostIp = {host}")
-#         sys.exit(1)
-
-#     if con.is_connected:
-#         print("远程节点连接创建成功")
-#         return con
-#     else:
-#         print(f"远程节点创建失败，hostIp = {host}")
-#         sys.exit(1)
-
-# # 设置默认编码为utf-8
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-# remote = genRemoteConnectionFunc("192.168.0.14","Aa1234561")
-# #不要显示run的输出
-# #res=remote.run("addr2line -a -e /root/mount/share/memtrace/sonia -f -C 0x000000000041022f", warn=True, echo=False,hide=True).stdout
-# res=remote.run("/root/mount/share/memtrace/sonia", warn=True, echo=False,hide=True).stdout
-# print("00000")
-# print(res)
-# print("33333")
-# # 输入数字0
-# user_input = input("请输入数字0: ")
-
-# res = remote.run(user_input, warn=True, echo=False, hide=True).stdout
-
-# print("11111")
-# print(res)
-# print("2222")
-
-# if remote is not None:
-#     print("\n开始关闭远程连接......\n")
-#     remote.close()
-#     print("\n远程连接关闭成功\n")
-# else:
-#     print("\n远程连接已关闭，无需手动关闭\n")
-
-
+import io
+from logging import warn
+from threading import local
+import sys
+import subprocess
+import time
+import importlib.util
 import paramiko
 import sys
 import io
 import time
+from PIL import Image
+
+#安装module
+def installPipModule(module):
+    if importlib.util.find_spec(module) is not None:
+        return
+    print(f"\n{module}模块未安装，先安装{module}模块\n")
+    try:
+        cmd = f"pip install {module}"
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"\n安装{module}模块时异常：\n", e.output)
+        sys.exit(1)
+    print(f"\n{module}模块安装成功\n")
+
+# # 先安装fabric模块
+# installPipModule("fabric")
+# from fabric import Connection
 
 # 设置默认编码为utf-8
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
@@ -113,20 +72,89 @@ if shell.recv_ready():
 #     output = shell.recv(1024).decode('utf-8')
 #     print(output)
 
-shell.send("python /root/mount/share/memtrace/server/erweima.py" + '\n')
-from PIL import Image
-def display_qr_image(image_data):
-    # 将二进制图像数据转换为 PIL 图像对象
-    img = Image.open(io.BytesIO(image_data))
+
+# 将ASCII二维码转换为图像
+def parse_ascii_to_image(ascii_data):
+    # 按行拆分 ASCII 数据
+    lines = ascii_data.splitlines()
+
+    # 确定图像大小
+    width, height = len(lines[0]), len(lines)
+
+    print(width, height)
+
+    # 创建黑白图片
+    img = Image.new('1', (width, height*2), color=1)  # '1'模式是1位黑白图像
+    pixels = img.load()
+
+    # 填充图像像素 0 黑色 1 白色
+    color1 = 1
+    color2 = 0
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            index = y*2
+            if char == '█':
+                pixels[x,index] = color1 
+                pixels[x, index+1] = color1
+            elif char == '▀':
+                pixels[x, index] = color1  # （显示上半部分）
+                pixels[x, index+1] = color2  # （显示下半部分）
+            elif char == '▄':
+                pixels[x,index] = color2  # （显示上半部分）
+                pixels[x, index+1] = color1  # （显示下半部分）
+            elif char == ' ':  # 不间断空格
+                pixels[x, index] = color2  
+                pixels[x, index+1] = color2
     
-    # 显示图像
-    img.show()
+    # 放大图像
+    scale = 30
+    new_width, new_height = width * scale, height * scale
+    img = img.resize((new_width, new_height), Image.NEAREST)  # NEAREST用于放大时保持像素的锐利边缘
+
+    return img
+
+#二进制数据中截取掉第一个\r\n前的数据以及最后一个\r\n后的数据
+def trim_binary_data(binary_data):
+    # 查找第一个 \r\n 的位置
+    first_crlf_pos = binary_data.find(b'\r\n')
+    
+    # 查找最后一个 \r\n 的位置
+    last_crlf_pos = binary_data.rfind(b'\r\n')
+
+    # 如果找到了 \r\n, 则进行截取
+    if first_crlf_pos != -1 and last_crlf_pos != -1 and first_crlf_pos != last_crlf_pos:
+        # 截取第一个 \r\n 后和最后一个 \r\n 前的数据
+        trimmed_data = binary_data[first_crlf_pos + 2:last_crlf_pos]
+        return trimmed_data
+    else:
+        # 如果没有找到，或数据无效，返回原数据
+        return binary_data
+
+
+shell.send('python3 /root/mount/share/memtrace/server/erweima.py' + '\n')
+
 
 # 读取初始提示符
 time.sleep(1)
 if shell.recv_ready():
-    output = shell.recv(1024)
-    display_qr_image(output)
+    output = shell.recv(8192)
+    #返回的二进制数据中截取掉''python3 /root/mount/share/memtrace/server/erweima.py\r\n'
+    #output = output[len('python3 /root/mount/share/memtrace/server/erweima.py\r\n'):]
+    
+    #返回的二进制数据中截取掉尾部的[root@vm14 ~]# 
+    #output = output[:-len('\r\n[root@vm14 ~]# ')]
+
+    #返回的二进制数据中截取掉第一个\r\n前的数据以及最后一个\r\n后的数据
+    output = trim_binary_data(output)
+    
+    output = output.decode('utf-8')
+    print(output)
+    #输出output的大小
+    print(len(output))
+    
+    # 将 ASCII 图案转为图片，可行
+    img = parse_ascii_to_image(output)
+    img.show()  # 展示图像
 
 
 # 关闭远程连接
@@ -136,3 +164,8 @@ if remote:
     print("\n远程连接关闭成功\n")
 else:
     print("\n远程连接已关闭，无需手动关闭\n")
+
+
+
+
+
